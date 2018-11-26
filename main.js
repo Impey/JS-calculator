8d523d398ea6784eaa10ac87507583cb0b123413
Tech-A42 <tech-a42@tech-a42s-air.home>
2018-11-26 21:29:34 +0000

    While loop implemented of sorts

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-var num1 = prompt("First Number");
-var op = prompt("Enter Operator");
-var num2 = prompt("Second Number");
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-var result;
+var calc = true;
+while (calc == true) {
+  var num1 = prompt("First Number");
+  var op = prompt("Enter Operator");
+  var num2 = prompt("Second Number");
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  var result;
 
 
-if (op == "+") {
-  result = num1 + num2;
-  alert(result);
+  if (op == "+") {
+    result = num1 + num2;
+    alert(result);
 
+  }
+  else if (op == "/") {
+    result = num1 / num2;
+    alert(result);
+  }
+  else if (op == "*") {
+
+    result = num1 * num2;
+    alert(result);
+  }
+  else if (op == "-") {
+    result = num1 - num2;
+    alert(result);
+  }
 }
-else if (op == "/") {
-  result = num1 / num2;
-  alert(result);
-}
-else if (op == "*") {
-
-  result = num1 * num2;
-  alert(result);
-}
-else if (op == "-") {
-  result = num1 - num2;
-  alert(result);
-}
-
